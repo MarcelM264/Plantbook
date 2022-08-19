6aaaadaaed4d8fa351c50c4bdfbd13ed6724414e
MarcelM264 <meyer1marcel@web.de>
2022-08-19 15:28:40 +0200

    added PlantPostingController + PlantPostingView

---
+++ Plantbook/Controllers/PlantPostingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Plantbook.Controllers
+{
+    public class PlantPostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditPlant(int id)
+        {
+            return View();
+        }
+    }
+}
